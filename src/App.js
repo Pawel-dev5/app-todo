402409e4e12ff9402f009a259dcb8a4870e3513b
Paweł <p.nowecki@gmail.com>
2021-02-06 16:03:52 +0100

    added sort and category filter function - value from select. Changed style

--- src/App.js
+++ src/App.js
@@ -4,36 +4,50 @@ import { useState } from "react";
 import * as _ from "lodash";
 
 function App() {
+  // Default modal state
   const defaultShow = false
   const [show, setShow] = useState(defaultShow);
   const [sumData, setSumData] = useState([]);
+  const [sumDataCopy, setSumDataCopy] = useState([]);
   const [data, setData] = useState([]);
-  const [sort, setSort] = useState("")
+  const [sort, setSort] = useState("");
+  const [filtr, setFiltr] = useState("");
+  console.log(sumData)
+  console.log(sumDataCopy)
+  console.log(data)
 
+  // Modal show/close function
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
+  
+  // Add value to Table
+  function add() {
+    let tab = [...sumData, data];
+    setShow(defaultShow);
+    return setSumData(tab);
+  }
+  // Get value form select to compare in sorted function
+  const setSortValue = (e) => {
+    setSort(e.target.value);
+  };
+  // Get value form select to compare in filtred function
+  const setFilterValue = (e) => {
+    setFiltr(e.target.value);
+  };
+  // Sort table by select
   function sorted() {
     const sorted = _.sortBy(sumData, sort)
     return setSumData(sorted)
   }
-
-  const setSortValue = (e) => {
-    setSort(e.target.value);
-  };
-  function a() {
-    let tab = [...sumData, data];
-    console.log(tab)
-    setShow(defaultShow);
-    return setSumData(tab);
-  }
-
+console.log(filtr)
   function filter(value) {
+    setSumDataCopy(sumData)
     const c = "Kryminał"
     setSumData(prevData => {
-      return prevData.filter((item) => value === item.category)
+      return prevData.filter((item) => filtr === item.category)
     })
   }
+  // Delete item from table
   function removeItem(id) {
     setSumData(prevData => {
       return prevData.filter((d, index) => {
@@ -41,10 +55,12 @@ function App() {
       })
     });
   }
-
+// Delete all by clear state
   function clearAll() {
     return setSumData([])
   }
+
+  // Handle modal form values
   const changeTitle = e => {
     const { value } = e.target;
     setData(prevState => ({
@@ -81,7 +97,7 @@ function App() {
         setSortValue={setSortValue}
         sumData={sumData}
         data={data}
-        a={a}
+        add={add}
         changeTitle={changeTitle}
         changeAutor={changeAutor}
         changeCat={changeCat}
@@ -91,6 +107,11 @@ function App() {
         show={show}
         removeItem={removeItem}
         clearAll={clearAll}
+        setSumData={setSumData}
+        sumDataCopy={sumDataCopy}
+        filter={filter}
+        setFilterValue={setFilterValue}
+        filtr={filtr}
       />
     </div>
   );
